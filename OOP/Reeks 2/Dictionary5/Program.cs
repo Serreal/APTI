51befcbac8214c62ed5b5287b3d1a96aeca6c434
Sander Standaert <standaertsander@gmail.com>
2020-02-21 15:27:13 +0100

    Dictionary Reeks 2

---
+++ OOP/Reeks 2/Dictionary5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> afkortingen = new Dictionary<string, string>();
+            afkortingen.Add("LOL", "Laughing Out Loud");
+        }
+    }
+}
