0bc20d1e0887ee713cea7fbc99dffe812fa622a2
Sander Standaert <standaertsander@gmail.com>
2020-02-21 15:27:13 +0100

    Dictionary Reeks 2

---
+++ OOP/Reeks 2/Dictionary5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> afkortingen = new Dictionary<string, string>();
+
+            afkortingen.Add("200", "OK");
+            afkortingen.Add("404", "Not Found");
+            afkortingen.Add("500", "Interne Serverfout");
+            afkortingen.Add("401", "Unauthorized");
+
+            Console.Write("Welke error code wil je opzoeken?: ");
+            string strinput = Console.ReadLine();
+
+            string value = "niet gevonden";
+
+            if (afkortingen.ContainsKey(strinput))
+            {
+                afkortingen.TryGetValue(strinput, out value);
+                Console.WriteLine($"Key {strinput} heeft value: {value}");
+            }
+            else
+            {
+                Console.WriteLine($"Key {strinput} {value}");
+            }
+        }
+    }
+}
