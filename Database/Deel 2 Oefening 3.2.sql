05709903876283981ebb261268b28ba3051938ed
Sander Standaert <standaertsander@gmail.com>
2020-03-04 15:38:22 +0100

    Database Deel 2

---
+++ Database/Deel 2 Oefening 3.2.sql
@@ -0,0 +1,6 @@
+select * into [dbo].[#tmp_table] from AdventureWorks2016CTP3.[Production].[Product];
+
+SELECT
+*
+FROM
+[dbo].[#tmp_table];
---
+++ Database/Deel 2 Oefening 3.3.sql
@@ -0,0 +1,8 @@
+create table [dbo].[altijdaanwezig](
+	pk int primary key
+	);
+
+create database test;
+
+drop table dbo.altijdaanwezig;
+drop database test;
